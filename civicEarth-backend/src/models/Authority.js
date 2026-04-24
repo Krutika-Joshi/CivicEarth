c67bfe8a2b7ecba1ac6f45ebad0794b1bec3cfa9
UniqueKJ <kru.t.ika04mjoshi1713@gmail.com>
2026-04-24 17:22:15 +0530

    updated

--- civicEarth-backend/src/models/Authority.js
+++ civicEarth-backend/src/models/Authority.js
@@ -1,26 +1,26 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require('mongoose');
 
 const authoritySchema = new mongoose.Schema({
-    name: {type: String, required: true },
+    name: { type: String, required: true },
 
     type: {
-    type: String,
-    enum: ["municipal", "pollution_board", "road", "police", "general"],
-    required: true
+        type: String,
+        enum: ["municipal", "pollution_board", "road", "police", "general"],
+        required: true
     },
 
-    jurisdiction: {type: String, required: true },
+    jurisdiction: { type: String, required: true },
 
-    email: {type: String, required: true },
+    email: { type: String, required: true },
 
     assignedReports: [
-        {type: mongoose.Schema.Types.ObjectId, ref: "Report"}
+        { type: mongoose.Schema.Types.ObjectId, ref: "Report" }
     ],
 
     level: {
         type: Number,
         required: true,
-        enum: [1,2,3]
+        enum: [1, 2, 3]
     }
 });
 
